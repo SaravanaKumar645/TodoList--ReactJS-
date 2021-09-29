4929e1925beb3f519fd4bac1faf4e27844a16832
SARAV <sarav023b@gmail.com>
2021-09-29 23:14:29 +0530

    Initial-Commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "./Header";
-//import List from "./TodoList/List";
 import Form from "./TodoForm/Form";
 function Todo() {
   return (
     <div>
       <Header />
       <Form />
-      {/* <List /> */}
     </div>
   );
 }
